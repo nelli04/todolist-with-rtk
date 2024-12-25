3b3bdcd1b268bd76d462dc48880f2183447423bb
Nelly_Ivanova <nelli_ivanova04@mail.ru>
2024-12-25 13:37:44 +0300

    feat(app): add app slice

---
+++ src/app/appSlice.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export type ThemeMode = "dark" | "light";
+export type RequestStatus = "idle" | "loading" | "succeeded" | "failed";
+
+export const appSlice = createSlice({
+  name: "app",
+  initialState: {
+    themeMode: "light" as ThemeMode,
+    status: "idle" as RequestStatus,
+    error: null as string | null,
+    isLoggedIn: false,
+  },
+  reducers: (create) => ({
+    changeTheme: create.reducer<{ themeMode: ThemeMode }>((state, action) => {
+      state.themeMode = action.payload.themeMode;
+    }),
+    setAppStatus: create.reducer<{ status: RequestStatus }>((state, action) => {
+      state.status = action.payload.status;
+    }),
+    setAppError: create.reducer<{ error: string | null }>((state, action) => {
+      state.error = action.payload.error;
+    }),
+    setIsLoggedIn: create.reducer<{ isLoggedIn: boolean }>((state, action) => {
+      state.isLoggedIn = action.payload.isLoggedIn;
+    }),
+  }),
+  selectors: {
+    selectThemeMode: (state) => state.themeMode,
+    selectAppStatus: (state) => state.status,
+    selectAppError: (state) => state.error,
+    selectIsLoggedIn: (state) => state.isLoggedIn,
+  },
+});
+
+export const { changeTheme, setAppError, setAppStatus, setIsLoggedIn } = appSlice.actions;
+export const { selectAppStatus, selectAppError, selectThemeMode, selectIsLoggedIn } = appSlice.selectors;
+export const appReducer = appSlice.reducer;
