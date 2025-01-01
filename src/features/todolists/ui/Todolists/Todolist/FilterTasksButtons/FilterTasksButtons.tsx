10c7b9f6ef3de41893c5c8b5597d118aaa74c031
Nelly_Ivanova <nelli_ivanova04@mail.ru>
2025-01-01 21:40:16 +0300

    feat(todolists): add filter tasks buttons component

---
+++ src/features/todolists/ui/Todolists/Todolist/FilterTasksButtons/FilterTasksButtons.tsx
@@ -0,0 +1,53 @@
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { useAppDispatch } from "common/hooks";
+import { todolistsApi } from "features/todolists/api/todolistsApi";
+import { filterButtonsContainerSx } from "./FilterTasksButtons.styles";
+import { DomainTodolist, FilterValues } from "features/todolists/lib/types/types";
+
+type Props = {
+  todolist: DomainTodolist;
+};
+
+export const FilterTasksButtons = ({ todolist }: Props) => {
+  const dispatch = useAppDispatch();
+
+  const changeFilterTasksHandler = (filter: FilterValues) => {
+    dispatch(
+      todolistsApi.util.updateQueryData("getTodolists", undefined, (state) => {
+        const index = state.findIndex((tl) => tl.id === id);
+        if (index !== -1) {
+          state[index].filter = filter;
+        }
+      }),
+    );
+  };
+
+  const { filter, id } = todolist;
+
+  return (
+    <Box sx={filterButtonsContainerSx}>
+      <Button
+        variant={filter === "all" ? "outlined" : "text"}
+        color={"inherit"}
+        onClick={() => changeFilterTasksHandler("all")}
+      >
+        All
+      </Button>
+      <Button
+        variant={filter === "active" ? "outlined" : "text"}
+        color={"primary"}
+        onClick={() => changeFilterTasksHandler("active")}
+      >
+        Active
+      </Button>
+      <Button
+        variant={filter === "completed" ? "outlined" : "text"}
+        color={"success"}
+        onClick={() => changeFilterTasksHandler("completed")}
+      >
+        Completed
+      </Button>
+    </Box>
+  );
+};
