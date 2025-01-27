8771f45d24c2b1c405cf36ca804c835b04fcf21d
Nelly_Ivanova <nelli_ivanova04@mail.ru>
2025-01-27 11:41:33 +0300

    refactor(theme): changes to improve code readability

--- src/common/theme/theme.ts
+++ src/common/theme/theme.ts
@@ -1,45 +1,44 @@
 import { amber, grey } from "@mui/material/colors";
-import { createTheme } from "@mui/material/styles";
+import { createTheme, Theme } from "@mui/material/styles";
 import { ThemeMode } from "app/appSlice";
 
-export const getTheme = (themeMode: ThemeMode) => {
-  return createTheme({
-    palette: {
-      ...(themeMode === "light"
-        ? {
-            primary: {
-              main: amber[500],
-            },
-            text: {
-              primary: grey[900],
-              secondary: grey[800],
-            },
-            background: {
-              default: amber[50],
-              paper: "#fffde7",
-            },
-            secondary: {
-              main: "#352f24",
-              light: "#ffc107"
-            },
-          }
-        : {
-            primary: {
-              main: grey[900],
-            },
-            background: {
-              default: grey[800],
-              paper: "#4c4c4c",
-            },
-            text: {
-              primary: "#fff",
-              secondary: grey[500],
-            },
-            secondary: {
-              main: "#9f9c96",
-              light: "#ffce58"
-            },
-          }),
-    },
-  });
+const lightPalette = {
+  primary: {
+    main: amber[500],
+  },
+  text: {
+    primary: grey[900],
+    secondary: grey[800],
+  },
+  background: {
+    default: amber[50],
+    paper: "#fffde7",
+  },
+  secondary: {
+    main: "#352f24",
+    light: "#ffc107",
+  },
 };
+
+const darkPalette = {
+  primary: {
+    main: grey[900],
+  },
+  text: {
+    primary: "#fff",
+    secondary: grey[500],
+  },
+  background: {
+    default: grey[800],
+    paper: "#4c4c4c",
+  },
+  secondary: {
+    main: "#9f9c96",
+    light: "#ffce58",
+  },
+};
+
+export const getTheme = (themeMode: ThemeMode): Theme =>
+  createTheme({
+    palette: themeMode === "light" ? lightPalette : darkPalette,
+  });
